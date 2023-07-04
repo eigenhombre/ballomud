2ee8ea83917369c3ad8491cf8035004e9430e689
John Jacobsen <eigenhombre@gmail.com>
2023-07-04 16:46:53 -0500

    Parse sections and rooms from config resource. #2

--- src/tableworld/parse.clj
+++ src/tableworld/parse.clj
@@ -1,8 +1,25 @@
 (ns tableworld.parse
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 (defn pull-sections [s]
   (into {} (map (juxt (comp keyword str/lower-case second)
                       #(nth % 2))
-                (re-seq #"(?m)^#\s*(.*?)\s*\n((?:.*\n)*?)(?=\n#|$)"
+                (re-seq #"#\s*(.*?)\s*\n((?:.*\n)*?)(?=\n#|$)"
                         s))))
+
+(defn- room-block [[id shortdesc-line & desclines]]
+  {:id id
+   :shortdesc (str/trim shortdesc-line)
+   :desc (str/join " " (map str/trim desclines))})
+
+(defn- parse-rooms [rooms]
+  (->> (str/split rooms #"\n")
+       (partition-by empty?)
+       (remove #{'("")})
+       (map room-block)))
+
+(comment
+  (let [secs (pull-sections (slurp (io/resource "world.tw")))]
+    (update secs :rooms parse-rooms)))
+
