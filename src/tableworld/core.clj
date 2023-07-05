9f59885ff3310dff4f713c8fe40cf33f3e6e5f10
John Jacobsen <eigenhombre@gmail.com>
2023-07-05 10:41:35 -0500

    Players persist and can be multiple.  #5

--- src/tableworld/core.clj
+++ src/tableworld/core.clj
@@ -6,27 +6,33 @@
             [clojure.string :as str]
             [tableworld.parse :as parse]))
 
-;; "accessors" for world:
-(defn location [world]
-  (get-in @world [:player :location]))
+;; For REPL hot reloading:
+(defonce live (atom false))
+(comment (reset! live true))
 
-(defn current-room [world]
-  (let [loc-id (location world)]
+
+;; "accessors" for world:
+(defn location [player-name world]
+  (get-in @world [:players player-name :location]))
+
+(defn current-room [player-name world]
+  (let [loc-id (location player-name world)]
     (get-in @world [:rooms loc-id])))
 
-(defn shortdesc [world]
-  (:shortdesc (current-room world)))
+(defn shortdesc [player-name world]
+  (:shortdesc (current-room player-name world)))
 
-(defn describe-location [world]
-  (:desc (current-room world)))
+(defn describe-location [player-name world]
+  (:desc (current-room player-name world)))
 
-(defn handle-command [command world]
+(defn handle-command [player-name command world]
   (cond
     (= command "help") "Available: look help hello time quit"
-    (= command "hello") "Hello!"
-    (= command "look") (describe-location world)
+    (= command "hello") (format "Hello, %s!" player-name)
+    (= command "dump") (pprint @world)  ;; hidden
+    (= command "look") (describe-location player-name world)
     (= command "time") (str "Current time is: " (java.util.Date.))
-    :else (format "I don't understand '%s'." command)))
+    :else (format "Sorry, %s, I don't understand '%s'." player-name command)))
 
 (defn is-quit? [cmd]
   (= cmd "quit"))
@@ -35,18 +41,36 @@
   (and (= (first s) (char 4))
        (= 1 (count s))))
 
+
+(defn disconnect [world player-name]
+  (swap! world update :players dissoc player-name))
+
 (defn accept [world]
   (loop []
-    (print ">>> ")
+    (print "What is your name? ")
     (flush)
-    (let [command (str/trim (str (read-line)))]
-      (cond
-        (iseof command) nil  ;; done
-        (seq command) (when-not (is-quit? command)
-                        (let [response (handle-command command world)]
-                          (println response)
-                          (recur)))
-        :else (recur)))))
+    (let [player-name (read-line)]
+      (if (get-in @world [:players player-name])
+        (do
+          (printf "Player name %s is taken!\n" player-name)
+          (flush)
+          (recur))
+        (do
+          (swap! world update :players assoc player-name {:location "hearth"
+                                                          :name player-name})
+          (printf "Welcome to Table World, %s.\n" player-name)
+          (loop []
+            (print ">>> ")
+            (flush)
+            (let [command (str/trim (str (read-line)))]
+              (cond
+                (iseof command) (disconnect world player-name)
+                (seq command) (if (is-quit? command)
+                                (disconnect world player-name)
+                                (let [response (handle-command player-name command world)]
+                                  (println response)
+                                  (recur)))
+                :else (recur)))))))))
 
 (defn start-server [name port daemon? world]
   (server/start-server {:name name
@@ -57,8 +81,7 @@
 
 (defn- main [name port daemon?]
   (let [world (atom
-               (assoc (parse/parse-world (slurp (io/resource "world.tw")))
-                      :player {:location "hearth"}))]
+               (parse/parse-world (slurp (io/resource "world.tw"))))]
     (print
      (format "Server name: '%s'  port: %d.  Accepting connections...."
              name port))
@@ -71,7 +94,7 @@
              port 9999}}]
   (main name port false))
 
-(comment
+(when @live
   (pprint @(atom
             (parse/parse-world (slurp (io/resource "world.tw")))))
   (server/stop-server "foo")
