29c4dd2c3438297b83211e11e58a11748979fd0f
John Jacobsen <eigenhombre@gmail.com>
2023-07-09 21:36:47 -0500

    Add second forest place and start async messages. #13

--- src/tableworld/core.clj
+++ src/tableworld/core.clj
@@ -12,34 +12,57 @@
 (defonce live (atom false))
 (comment (reset! live true))
 
+(defn remove-edge-quotes [s]
+  (-> s
+      (str/replace #"^(?:\'|\")" "")
+      (str/replace #"(?:\'|\")$" "")))
+
+(defn say [player-name args world]
+  (format "You say: '%s'.\n" (->> args
+                                  (map remove-edge-quotes)
+                                  (str/join " "))))
+
 (defn handle-command [player-name command world]
-  (cond
-    (= command "help") (str "Available: look help hello time dump quit
-n s e w north south east west")
-    (= command "hello") (format "Hello, %s!" player-name)
-    (= command "dump") (pprint @world)
-    (= command "look") (m/describe-player-location player-name
-                                                   @world
-                                                   :detailed)
-    (= command "time") (str "Current time is: " (java.util.Date.))
-    (#{"e" "east"
-       "n" "north"
-       "s" "south"
-       "w" "west"} command)
-    (let [{:keys [error status]}
-          (m/try-to-move-player!
-           player-name
-           (keyword (get {"east" "e"
-                          "north" "n"
-                          "south" "s"
-                          "west" "w"} command command))
-           world)]
-      (if (= status :fail)
-        (get m/ttmp-error-descriptions error "Unknown error")
-        (m/describe-player-location player-name @world)))
-    :else (format "Sorry, %s, I don't understand '%s'. Type 'help' for help."
-                  player-name
-                  command)))
+  (let [[command & args] (str/split command #"\s")]
+    (cond
+      (= command "help") "Available:
+    dump
+    hello
+    help
+    look
+    quit
+    say
+    time
+    n     or north
+    s     or south
+    e     or east
+    w     or west
+"
+      (= command "hello") (format "Hello, %s!" player-name)
+      (= command "dump") (pprint @world)
+      (= command "look") (m/describe-player-location player-name
+                                                     @world
+                                                     :detailed)
+      (= command "time") (str "Current time is: " (java.util.Date.))
+      (= command "say") (say player-name args world)
+      (#{"e" "east"
+         "n" "north"
+         "s" "south"
+         "w" "west"} command)
+      (let [{:keys [error status]}
+            (m/try-to-move-player!
+             player-name
+             (keyword (get {"east" "e"
+                            "north" "n"
+                            "south" "s"
+                            "west" "w"} command command))
+             world)]
+        (if (= status :fail)
+          (get m/ttmp-error-descriptions error "Unknown error")
+          (m/describe-player-location player-name @world)))
+      :else (format "Sorry, %s, I don't understand '%s'. Type 'help' for help."
+                    player-name
+                    command))))
 
 (defn is-quit? [cmd]
   (= cmd "quit"))
@@ -57,15 +80,57 @@ n s e w north south east west")
 (defn wrap [s]
   (wrap/wrap-indent s 50 2))
 
-(defn accept [world]
-  (splash)
+(defn do-something-random-later []
+  (future
+    (Thread/sleep (rand-int 10000))
+    (when (zero? (rand-int 3))
+      (println
+       (str "\n"
+            (wrap (rand-nth ["You hear a soft breeze blowing."
+                             "Your stomach grumbles."
+                             "A fly buzzes unseen nearby."
+                             "Someone coughs."]))))
+      (print ">>> ")
+      (flush))))
+
+(defn do-player-loop [player-name world]
+  (printf "Welcome to Table World, %s.\n" player-name)
+  (m/add-player! player-name "hearth" world)
+  (println (wrap (m/describe-player-location player-name
+                                             @world
+                                             :detailed)))
   (loop []
-    (print "Are you a bot?  Type 'n' or 'no' if not... ")
+    (print ">>> ")
     (flush)
-    (when (#{"n" "no"} (-> (read-line) str/trim str/lower-case))
-      (print "What is your name? ")
-      (flush)
-      (let [player-name (str/trim (read-line))]
+    (let [command (str/trim (str (read-line)))]
+      (cond
+        (iseof command) (disconnect world player-name)
+        (seq command) (if (is-quit? command)
+                        (disconnect world player-name)
+                        (let [response (handle-command player-name
+                                                       command
+                                                       world)]
+                          (println (wrap response))
+                          (do-something-random-later)
+                          (recur)))
+        :else (recur)))))
+
+(defn accept [skip-intro? world]
+  (splash)
+  (when-not skip-intro?
+    (print "Are you a bot?  Type 'n' or 'no' if not... ")
+    (flush))
+  (loop []
+    (when (or skip-intro?
+              (#{"n" "no"} (-> (read-line)
+                               str/trim
+                               str/lower-case)))
+      (when-not skip-intro?
+        (print "What is your name? ")
+        (flush))
+      (let [player-name (if skip-intro?
+                          "Mr.Tester"
+                          (str/trim (read-line)))]
         (cond
           (re-find #"\s" player-name)
           (do
@@ -81,51 +146,32 @@ n s e w north south east west")
           (empty? player-name) (recur)
 
           :else
-          (do
-            (printf "Welcome to Table World, %s.\n" player-name)
-            (m/add-player! player-name "hearth" world)
-            (println (wrap (m/describe-player-location player-name
-                                                       @world
-                                                       :detailed)))
-            (loop []
-              (print ">>> ")
-              (flush)
-              (let [command (str/trim (str (read-line)))]
-                (cond
-                  (iseof command) (disconnect world player-name)
-                  (seq command) (if (is-quit? command)
-                                  (disconnect world player-name)
-                                  (let [response (handle-command player-name
-                                                                 command
-                                                                 world)]
-                                    (println (wrap response))
-                                    (recur)))
-                  :else (recur))))))))))
+          (do-player-loop player-name world))))))
 
-(defn start-server [host port daemon? world]
+(defn start-server [host port daemon? skip-intro? world]
   (server/start-server {:name "tableworld"
                         :address host
                         :port port
                         :accept 'tableworld.core/accept
-                        :args [world]
+                        :args [skip-intro? world]
                         :server-daemon daemon?}))
 
-(defn- main [host port daemon?]
+(defn- main [host port daemon? skip-intro?]
   (let [world (atom
                (parse/parse-world (slurp (io/resource "world.tw"))))]
     (print
      (format "Server name: '%s'  port: %d.  Accepting connections...."
              host port))
     (flush)
-    (start-server host port daemon? world)))
+    (start-server host port daemon? skip-intro? world)))
 
-(defn -main [& [host port]]
+(defn -main [& [host port skip-intro?]]
   (let [host (or host "localhost")
         port (Integer. (or port "9999"))]
-    (main host port false)))
+    (main host port false false)))
 
 (when @live
   (pprint @(atom
             (parse/parse-world (slurp (io/resource "world.tw")))))
   (server/stop-server "tableworld")
-  (main "localhost" 9999 true))
+  (main "localhost" 9999 true true))
