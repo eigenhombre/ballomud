852893f86cd7676184032b94f8520ee7ad7a672d
John Jacobsen <eigenhombre@gmail.com>
2024-10-05 11:57:43 -0500

    Finish(?) flow, minus Docker push. #26

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ballomud "0.0.1"
+(defproject ballomud "0.0.2-SNAPSHOT"
   :description "A small MUD-inspired game"
   :url "https://github.com/eigenhombre/ballomud"
   :license {:name "MIT"}
@@ -23,4 +23,7 @@
                   ["vcs" "commit"]
                   ["vcs" "tag" "v" "--no-sign"]
                   ["uberjar"]
-                  ])
+                  ;; TODO: push to docker
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]
