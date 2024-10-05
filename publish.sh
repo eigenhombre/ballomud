09b80421926adeed5b9e2490a4647607588829bd
John Jacobsen <eigenhombre@gmail.com>
2024-10-05 12:12:35 -0500

    Add publish script. #26

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+VERSION=v$(lein pprint --no-pretty -- :version)
+
+echo "About to push $VERSION..."
+
+docker build -t ballomud --force-rm \
+       -t eigenhombre/ballomud:${VERSION} \
+       -t eigenhombre/ballomud:latest .
