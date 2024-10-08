a3bbadf4d63f1b389d168452c3c42a27d4b76fdb
John Jacobsen <eigenhombre@gmail.com>
2024-10-07 20:36:23 -0500

    Version 0.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ballomud "0.0.11-SNAPSHOT"
+(defproject ballomud "0.0.11"
   :description "A small MUD-inspired game"
   :url "https://github.com/eigenhombre/ballomud"
   :license {:name "MIT"}
